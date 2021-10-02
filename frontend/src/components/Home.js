6f06501b0e1ba7a6fc90dc2f02785e9ec1274fab
Reese Hoffart <reese@Reeses-MacBook-Air.local>
2021-10-02 12:13:59 -0500

    cant view home page without signing in thru cookies

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import logo from '../workload.png'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
+
+class Home extends React.Component {
+  render() {
+    return(
+      <p>hi</p>
+    )
+  }
+}
+
+export default Home
